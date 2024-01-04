b683078a8e96e0e99c2446f45d4367bea25967c7
Librariann <okpc0306@naver.com>
2024-01-04 20:29:51 +0900

    [commit] add spinner component to react-quiz-app

--- quiz-app/react-quiz/src/components/Spinner/index.tsx
+++ quiz-app/react-quiz/src/components/Spinner/index.tsx
@@ -1,7 +1,30 @@
+import { Spinner } from "@chakra-ui/react";
 import React from "react";
 
-const Spinner = () => {
-  return <div>Spinner</div>;
+interface SpinnerProps {
+  thickness: string;
+  speed: string;
+  color: string;
+  emptyColor: string;
+  size: string;
+}
+
+const AppSpinner: React.FC<SpinnerProps> = ({
+  thickness,
+  speed,
+  color,
+  emptyColor,
+  size,
+}) => {
+  return (
+    <Spinner
+      thickness={thickness}
+      speed={speed}
+      color={color}
+      emptyColor={emptyColor}
+      size={size}
+    />
+  );
 };
 
-export default Spinner;
+export default AppSpinner;
