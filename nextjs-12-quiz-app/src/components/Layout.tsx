0fb4258a117926739927697d5beda4f031e76b74
Librariann <okpc0306@naver.com>
2024-01-08 21:42:39 +0900

    [commit] add styling next12-quiz-app

--- nextjs-12-quiz-app/src/components/Layout.tsx
+++ nextjs-12-quiz-app/src/components/Layout.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
-const Layout = () => {
-  return <div>Layout</div>;
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Header />
+      <main className="main">{children}</main>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
