9ac957bf62a6f474a36ecd59950b65a450ead7b9
Librariann <okpc0306@naver.com>
2024-01-09 21:59:07 +0900

    [commit] add quiz data and Quiz page next12-quiz-app

---
+++ nextjs-12-quiz-app/src/pages/api/quiz.ts
@@ -0,0 +1,58 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const questionAndAnswers = [
+  {
+    id: 1,
+    question: "1+1 ?",
+    options: ["1", "2", "3", "4"],
+    answer: "2",
+  },
+  {
+    id: 2,
+    question: "2+2 ?",
+    options: ["1", "2", "3", "4"],
+    answer: "4",
+  },
+];
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { page } = req.query as { page: string };
+    if (!page) {
+      res.send(questionAndAnswers);
+    }
+
+    let pageToNum = parseInt(page);
+    const quiz = questionAndAnswers[pageToNum];
+    const lastPage = questionAndAnswers.length - 1;
+    const numOfQuestion = questionAndAnswers.length;
+
+    if (pageToNum === 0) {
+      res.send({
+        prev: false,
+        next: true,
+        page,
+        quiz,
+        total: numOfQuestion,
+      });
+    } else if (pageToNum === lastPage) {
+      res.send({
+        prev: true,
+        next: false,
+        page,
+        quiz,
+        total: numOfQuestion,
+      });
+    } else {
+      res.send({
+        prev: true,
+        next: true,
+        page,
+        quiz,
+        total: numOfQuestion,
+      });
+    }
+  } catch (e) {
+    res.status(500);
+  }
+}
