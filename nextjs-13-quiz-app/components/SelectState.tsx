67a57bfdf3154da79c764d2a947dbbc4edf3b6c3
Librariann <okpc0306@naver.com>
2024-01-23 10:00:10 +0900

    [commit] add top select box from state page for next13-quiz-app

--- nextjs-13-quiz-app/components/SelectState.tsx
+++ nextjs-13-quiz-app/components/SelectState.tsx
@@ -1,7 +1,24 @@
+import { IState } from "@/utils/types";
 import React from "react";
 
-const SelectState = () => {
-  return <div>SelectState</div>;
+interface Props {
+  states: IState[];
+  state: (stateValue: number) => void;
+}
+
+const SelectState = ({ states, state }: Props) => {
+  return (
+    <select
+      className={`mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm`}
+      onChange={(e) => state(Number(e.target.value))}
+    >
+      {states.map((state) => (
+        <option key={state.id} value={state.id - 1}>
+          {state.title}
+        </option>
+      ))}
+    </select>
+  );
 };
 
 export default SelectState;
--- nextjs-13-quiz-app/components/State.tsx
+++ nextjs-13-quiz-app/components/State.tsx
@@ -13,6 +13,7 @@ const State = ({ states }: Props) => {
   const [state, setState] = useState(0);
   return (
     <div>
+      <SelectState states={states} state={(value) => setState(value)} />
       <div className="flex flex-col space-y-4 md:space-y-8 pt-4">
         {states[state].questions.map((question) => (
           <Question
