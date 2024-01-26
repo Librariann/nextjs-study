86791f1194dddbf3535d7ca79b5b3c6570a27eb3
Librariann <okpc0306@naver.com>
2024-01-26 10:00:10 +0900

    [commit] quiz pageForm for next13-quiz-app

--- nextjs-13-quiz-app/components/QuizForm.tsx
+++ nextjs-13-quiz-app/components/QuizForm.tsx
@@ -3,6 +3,7 @@ import { IQuestion } from "@/utils/types";
 import React, { useState } from "react";
 import SelectState from "./SelectState";
 import { getQuizQuestions, getStates } from "@/utils/functions";
+import Question from "./Question";
 
 const QuizForm = () => {
   const [state, setState] = useState(1);
@@ -19,6 +20,16 @@ const QuizForm = () => {
     setQuizStarted(true);
   };
 
+  const quizEnd = () => {
+    setQuizEnded(true);
+  };
+
+  const nextQuestion = () => {
+    setCurrentQuestion(currentQuestion + 1);
+    setAnswers([...answers, currentAnswer]);
+    setAnswered(false);
+  };
+
   return (
     <div className="flex flex-col space-y- md:space-y-8 p-4 lg:p-8">
       <h1 className="text-4xl font-semibold pb-4 mb-4 border-b border-gray-300">
@@ -31,9 +42,43 @@ const QuizForm = () => {
             states={getStates()}
             state={(stateValue) => setState(stateValue)}
           />
-          <button className="my-4 px-4 py-2 text-white rounded-lg shadow bg-blue-600 hover:bg-blue-500">
+          <button
+            className="my-4 px-4 py-2 text-white rounded-lg shadow bg-blue-600 hover:bg-blue-500"
+            onClick={startQuiz}
+          >
             연습 테스트 시작
           </button>
+        </div>
+      )}
+
+      {quizStarted && !quizEnded && (
+        <div>
+          <Question
+            question={questions[currentQuestion]}
+            getAnswer={(answer) => {
+              setCurrentAnswer(answer);
+              setAnswered(true);
+            }}
+            withId={false}
+            checkEnabled={false}
+          />
+          <div>
+            <div>
+              문제 {currentQuestion + 1} / {questions.length}
+            </div>
+            {answered && (
+              <button
+                className="my-4 px-8 py-2 text-white float-right rounded-lg shadow bg-blue-600 hover:bg-blue-500"
+                onClick={() =>
+                  currentQuestion > questions.length - 1
+                    ? quizEnd()
+                    : nextQuestion()
+                }
+              >
+                {currentQuestion > questions.length - 1 ? "완료" : "다음"}
+              </button>
+            )}
+          </div>
         </div>
       )}
     </div>
