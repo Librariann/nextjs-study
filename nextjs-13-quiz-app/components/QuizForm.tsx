e1dad79d38736e0cc8d7b3a9902fa42c95f21702
Librariann <okpc0306@naver.com>
2024-01-27 10:00:10 +0900

    [commit] complete quiz page for next13-quiz-app

--- nextjs-13-quiz-app/components/QuizForm.tsx
+++ nextjs-13-quiz-app/components/QuizForm.tsx
@@ -22,6 +22,7 @@ const QuizForm = () => {
 
   const quizEnd = () => {
     setQuizEnded(true);
+    setAnswers([...answers, currentAnswer]);
   };
 
   const nextQuestion = () => {
@@ -30,11 +31,25 @@ const QuizForm = () => {
     setAnswered(false);
   };
 
+  const passed = () => answers.filter((a) => a).length >= 2;
+  const newStart = () => {
+    setQuizStarted(false);
+    setAnswered(false);
+    setCurrentAnswer(false);
+    setQuizEnded(false);
+    setState(1);
+    setCurrentQuestion(0);
+    setQuestions([]);
+    setAnswers([]);
+  };
+
   return (
     <div className="flex flex-col space-y- md:space-y-8 p-4 lg:p-8">
       <h1 className="text-4xl font-semibold pb-4 mb-4 border-b border-gray-300">
         시험
       </h1>
+
+      {/* 처음화면 상태 */}
       {!quizStarted && (
         <div className="bg-slate-50 p-6 rounded-xl shadow">
           <h2 className="text-lg">상태선택</h2>
@@ -51,6 +66,7 @@ const QuizForm = () => {
         </div>
       )}
 
+      {/* 퀴즈 시작했을때 */}
       {quizStarted && !quizEnded && (
         <div>
           <Question
@@ -70,15 +86,41 @@ const QuizForm = () => {
               <button
                 className="my-4 px-8 py-2 text-white float-right rounded-lg shadow bg-blue-600 hover:bg-blue-500"
                 onClick={() =>
-                  currentQuestion > questions.length - 1
+                  currentQuestion >= questions.length - 1
                     ? quizEnd()
                     : nextQuestion()
                 }
               >
-                {currentQuestion > questions.length - 1 ? "완료" : "다음"}
+                {currentQuestion >= questions.length - 1 ? "완료" : "다음"}
               </button>
             )}
           </div>
+        </div>
+      )}
+
+      {quizEnded && (
+        <div className="bg-slate-50 p-8 rounded-xl shadow">
+          <div className="text-4xl font-semibold mb-8 ">결과</div>
+          <div>
+            <span className="font-bold">{questions.length}</span> 점 중{" "}
+            <span
+              className={`font-bold ${
+                passed() ? "text-green-600" : "text-red-600"
+              }`}
+            >
+              {answers.filter((a) => a).length}
+            </span>{" "}
+            점을 획득했습니다.
+          </div>
+          <div className="my-4">
+            {passed() ? "축하합니다" : "시험에 합격하지 못했습니다."}
+          </div>
+          <button
+            onClick={newStart}
+            className={`my-4 px-8 py-2 text-white w-full rounded-lg shadow bg-blue-600 hover:bg-blue-500`}
+          >
+            새로운 연습 테스트를 시작하세요.
+          </button>
         </div>
       )}
     </div>
