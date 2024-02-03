0df040e35fd9790dfda377fc7b06c5e9508d7e64
Librariann <okpc0306@naver.com>
2024-02-03 10:00:10 +0900

    [commit] add react-query app for react-weather-app

--- react-weather/src/App.tsx
+++ react-weather/src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  return <div className="App"></div>;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/:id" element={<Home />} />
+      </Routes>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
