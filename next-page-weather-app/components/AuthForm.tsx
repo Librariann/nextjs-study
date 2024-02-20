11a1e99070987683fc79a6bd3777271d3c289cc0
Librariann <okpc0306@naver.com>
2024-02-20 10:00:10 +0900

    [commit] add Singup for next-page-weather-app

--- next-page-weather-app/components/AuthForm.tsx
+++ next-page-weather-app/components/AuthForm.tsx
@@ -1,8 +1,26 @@
 import { useRouter } from "next/router";
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import InputContainer from "./InputContainer";
 import emailIcon from "@/public/email_envelope_mail_send_icon.svg";
 import lockIcon from "@/public/lock_locker_icon.svg";
+import { User } from "@/models/customTypes";
+
+async function registerUser(userData: User) {
+  const response = await fetch("api/auth/signup", {
+    method: "POST",
+    body: JSON.stringify(userData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.method || "Something went wrong");
+  }
+
+  return data;
+}
 
 const AuthForm = () => {
   const [enteredEmail, setEnteredEmail] = useState("");
@@ -18,8 +36,35 @@ const AuthForm = () => {
     setEnteredConfirmPassword("");
   };
 
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const userData: User = { enteredEmail, enteredPassword };
+
+    if (
+      enteredEmail?.trim().length === 0 ||
+      enteredPassword?.trim().length === 0
+    ) {
+      alert("먼저 유저 이름과 비밀번호를 입력해주세요.");
+      return;
+    }
+
+    if (!isLogin) {
+      if (enteredPassword !== enteredConfirmPassword) {
+        alert("비밀번호가 일치하지 않습니다.");
+        return;
+      }
+      try {
+        const response = await registerUser(userData);
+        alert("회원가입에 성공했습니다.");
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+    }
+  };
+
   return (
-    <form className="auth__form">
+    <form className="auth__form" onSubmit={submitHandler}>
       <InputContainer
         type="email"
         value={enteredEmail}
